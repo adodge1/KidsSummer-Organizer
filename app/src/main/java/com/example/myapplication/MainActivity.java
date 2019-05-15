d1aa64d2875af89589f1912e847c43aecebb5c57
Adriana Dodge <adriana.jimenez@neulioncollege.com>
2019-05-15 09:31:18 -0400

    Organized imports

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,31 +3,18 @@ package com.example.myapplication;
 
 
 import android.content.Intent;
-
-
-
 import android.support.annotation.NonNull;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Toast;
-
 import com.example.myapplication.Utils.FirebaseDBUtil;
 import com.example.myapplication.model.Child;
-
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-
 import java.util.ArrayList;
 
 
