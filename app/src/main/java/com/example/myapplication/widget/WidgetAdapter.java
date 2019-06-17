6375ccc019c342ed690a9f8cbe9520d09794ad4b
Adriana Dodge <adriana.jimenez@neulioncollege.com>
2019-06-17 11:22:25 -0400

    Added functions to get data from firebase

--- app/src/main/java/com/example/myapplication/widget/WidgetAdapter.java
+++ app/src/main/java/com/example/myapplication/widget/WidgetAdapter.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.widget;
 
+import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 
@@ -24,32 +25,40 @@ import java.util.ArrayList;
 public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory  {
     private Context mContext;
 
-    private DatabaseReference childrenRef;
-    private ValueEventListener valueEventListener;
+    private int appWidgetId;
 
-    private ArrayList<Camp> camps = new ArrayList<>();
+
     private ArrayList<Child> kids = new ArrayList<>();
 
-    private  DatabaseReference rootRef = FirebaseDBUtil.getDatabase().getReference();
+    private ArrayList<Camp> kidCamps = new ArrayList<>();
+    private String mCapmInfo = "";
 
 
-   //https://medium.com/@darshankawar/displaying-data-in-app-widget-screen-with-firebase-ed7e892b290c
+    //https://medium.com/@darshankawar/displaying-data-in-app-widget-screen-with-firebase-ed7e892b290c
 
 
 
 
-    public WidgetAdapter(Context context){
+    public WidgetAdapter(Context context,Intent intent){
         this.mContext = context;
+        appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+
+        getKidsNamesDB();
+
+
     }
 
     @Override
     public void onCreate() {
-        initializeData();
+
     }
 
     @Override
     public void onDataSetChanged() {
-      //  initializeData();
+
+        getKidsNamesDB();
+
     }
 
     @Override
@@ -59,6 +68,13 @@ public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory  {
 
     @Override
     public int getCount() {
+        try {
+            Thread.currentThread();
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Log.d("TAG", "Total count is " +kids.size());
         return kids == null ? 0 : kids.size();
     }
@@ -66,16 +82,26 @@ public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory  {
     @Override
     public RemoteViews getViewAt(final int position) {
 
-       /* if (position == AdapterView.INVALID_POSITION ) {
-            return null;
-        }*/
-
         final RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_item);
-
         remoteViews.setTextViewText(R.id.widget_tv,kids.get(position).getChildName());
-         Intent intent =new Intent();
-        intent.putExtra(WidgetProvider.WIDGET_KEY_ITEM,kids.get(position).getChildName());
-       remoteViews.setOnClickFillInIntent(R.id.widget_rv, intent);
+        Intent intent =new Intent();
+
+
+        getCampsOfKidDB(kids.get(position).getChildName());
+        mCapmInfo ="";
+        Log.d("TAG", "*****************************KIDS CAMP************************** " +kidCamps.size());
+        for (int i = 0; i < kidCamps.size(); i++) {
+
+            // accessing each element of array
+            mCapmInfo = mCapmInfo +" "+ kidCamps.get(i).getCampName() +" - " +kidCamps.get(i).getWeekFrom()+"\n";
+
+        }
+
+
+
+        intent.putExtra(WidgetProvider.WIDGET_KEY_ITEM, mCapmInfo);
+        mCapmInfo ="";
+        remoteViews.setOnClickFillInIntent(R.id.widget_rv, intent);
         return remoteViews;
     }
 
@@ -101,19 +127,23 @@ public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory  {
     }
 
 
-    private void initializeData(){
 
 
-        childrenRef= rootRef.child("kids");
+    private void getKidsNamesDB() {
+
+
+        DatabaseReference rootRef = FirebaseDBUtil.getDatabase().getReference();
+        DatabaseReference childrenRef= rootRef.child("kids");
         childrenRef.keepSynced(true);
+
 
         Log.d("TAG", "childrenRef " +childrenRef.toString());
 
 
-        valueEventListener = new ValueEventListener() {
+        ValueEventListener  valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                kids.clear();
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String childName = ds.child("childName").getValue(String.class);
                     String childAge = ds.child("age").getValue(String.class);
@@ -134,13 +164,42 @@ public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory  {
         };
 
         childrenRef.addValueEventListener(valueEventListener);
-
-
-
     }
 
+    private void getCampsOfKidDB(String kidName) {
 
 
 
+
+        DatabaseReference rootRef = FirebaseDBUtil.getDatabase().getReference();
+        DatabaseReference kidsCampsRef = rootRef.child("kid_camps").child(kidName);
+        kidsCampsRef.orderByValue();
+        kidsCampsRef.keepSynced(true);
+
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                kidCamps.clear();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    String campName = ds.getKey();
+                    String campWeekFrom = ds.getValue(String.class);
+
+                    Camp newCamp = new Camp(campName,campWeekFrom);
+                    kidCamps.add(newCamp);
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        };
+
+        kidsCampsRef.addValueEventListener(valueEventListener);
+
+    }
 
 }
